e29f72116590fe63bfc599a35dea30dac083246f
ruly-ooka <ayana.ruly@outlook.jp>
2026-06-05 09:07:01 +0900

    FirstStepJdbcUpdate_02,FirstStepJdbcDeleteクラスの実装

---
+++ src/main/java/jp/ruly/lecture/db/FirstStepJdbcUpdate.java
@@ -0,0 +1,42 @@
+package jp.ruly.lecture.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class FirstStepJdbcUpdate {
+	private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "postgres";
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("ユーザーの更新");
+			System.out.println("名前の入力");
+			String name = scanner.nextLine();
+
+			System.out.println("メールアドレスを入力");
+			System.out.println("データの更新を開始");
+			String email = scanner.nextLine();
+
+			System.out.println("--------------------------------------------------");
+
+			String sql = "UPDATE \"ユーザーテーブル\" SET \"氏名\" = ?, \"メールアドレス\" = ? WHERE \"ユーザーID\" = 1";
+			try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+					PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+				pstmt.setString(1, name);
+				pstmt.setString(2, email);
+
+				int count = pstmt.executeUpdate();
+
+				System.out.println("--------------------------------------------------");
+				System.out.println("データ更新が完了（更新件数：" + count + "）");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/main/java/jp/ruly/lecture/db/FirstStepJdbcUpdate_02.java
@@ -0,0 +1,41 @@
+package jp.ruly.lecture.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class FirstStepJdbcUpdate_02 {
+
+	private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "postgres";
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("アイテム更新");
+			System.out.println("アイテムの名前を入力");
+			String name = scanner.nextLine();
+
+			System.out.println("アイテムの金額を入力");
+			int price = scanner.nextInt();
+
+			System.out.println("データの更新を開始");
+			System.out.println("--------------------------------------------------");
+
+			String sql = "UPDATE items SET name = ?, price = ? WHERE id = 1";
+			try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+				PreparedStatement pstmt = con.prepareStatement(sql);
+
+				pstmt.setString(1, name);
+				pstmt.setInt(2, price);
+
+				int count = pstmt.executeUpdate();
+				System.out.println("データの更新完了（更新件数：" + count + "件）");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
