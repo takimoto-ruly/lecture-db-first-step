ab7e6c1e5d38deb5ca2d690b0b2c09ae79ff0f7b
ruly-ooka <ayana.ruly@outlook.jp>
2026-06-16 10:11:11 +0900

    課題myBatis演習実習の修正

--- src/main/java/jp/ruly/lecture/db/mybatis/training/basic/mb01/entity/User.java
+++ src/main/java/jp/ruly/lecture/db/mybatis/training/basic/mb01/entity/User.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class User {
-
 	private Integer id;
 	private String name;
 	private Integer age;
---
+++ src/main/resources/jp/ruly/lecture/db/mybatis/training/basic/mb01/repository/package-info.java
@@ -0,0 +1,1 @@
+package jp.ruly.lecture.db.mybatis.training.basic.mb01.repository;
